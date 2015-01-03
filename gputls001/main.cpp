40b248ab77db8b37fd353bd713971b7e1abd5142
liuhongyuan <lliuhy@gmail.com>
2015-01-03 14:25:48 +0800

    Initial Commit

---
+++ gputls001/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gputls001
+//
+//  Created by LIU Hongyuan on 1/3/15.
+//  Copyright (c) 2015 bigthree. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
