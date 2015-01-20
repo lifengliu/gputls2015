4fb5b64beb5fe123b1cb1b436a294b10b88a30ef
liuhongyuan <lliuhy@gmail.com>
2015-01-20 11:44:16 +0800

    edit test

--- gputls001/main.cpp
+++ gputls001/main.cpp
@@ -79,9 +79,9 @@ static int sequentialTest1(double *used_time) {
 }
 
 
-static int specTest1NoDependencies(double *used_time) {
+static int specTest1NoDependencies(double *used_time, int dc_on = 1) {
 
-	cl_device_id device = gputls::getOneGPUDevice(0);
+	cl_device_id device = gputls::getOneGPUDevice(1);    // 0 is APU; 1 is R9 290X
 	//printfunc::display_device(device);
 
 	cl_int clStatus;
@@ -162,91 +162,96 @@ static int specTest1NoDependencies(double *used_time) {
 	puts("this finishes speculative execution, next we perform dependency checking \n\n\n");
 	 */
 
+	if (dc_on) {
+		// phase 1
+		clSetKernelArg(DC1kernel, 0, sizeof(cl_mem), &device_read_trace);
+		clSetKernelArg(DC1kernel, 1, sizeof(cl_mem), &device_write_trace);
+		clSetKernelArg(DC1kernel, 2, sizeof(cl_mem), &device_read_to);
+		clSetKernelArg(DC1kernel, 3, sizeof(cl_mem), &device_write_to);
+		clSetKernelArg(DC1kernel, 4, sizeof(cl_mem), &device_write_count);
 
-	// phase 1
-	clSetKernelArg(DC1kernel, 0, sizeof(cl_mem), &device_read_trace);
-	clSetKernelArg(DC1kernel, 1, sizeof(cl_mem), &device_write_trace);
-	clSetKernelArg(DC1kernel, 2, sizeof(cl_mem), &device_read_to);
-	clSetKernelArg(DC1kernel, 3, sizeof(cl_mem), &device_write_to);
-	clSetKernelArg(DC1kernel, 4, sizeof(cl_mem), &device_write_count);
+		clStatus = clEnqueueNDRangeKernel(command_queue, DC1kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
 
-	clStatus = clEnqueueNDRangeKernel(command_queue, DC1kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
+		//int tmp_output[100];
+		//clEnqueueReadBuffer(command_queue, device_write_count, CL_TRUE, 0, 100 * sizeof(int), tmp_output, 0, NULL, NULL);
 
-	//int tmp_output[100];
-	//clEnqueueReadBuffer(command_queue, device_write_count, CL_TRUE, 0, 100 * sizeof(int), tmp_output, 0, NULL, NULL);
+		/*for (int i = 0; i < 100; i++) {
+			printf("%d ", tmp_output[i]);
+		}
 
-	/*for (int i = 0; i < 100; i++) {
-		printf("%d ", tmp_output[i]);
-	}
+		puts("");
+		*/
 
-	puts("");
-	*/
+		//reduction on device_write_to
 
-	//reduction on device_write_to
+		size_t localWorkSize = 128;
+		size_t numWorkGroups = 64;
+		size_t globalWorkSize = numWorkGroups * localWorkSize;
 
-    size_t localWorkSize = 128;
-    size_t numWorkGroups = 64;
-    size_t globalWorkSize = numWorkGroups * localWorkSize;
+		cl_mem device_reducedWriteTo = clCreateBuffer(context, CL_MEM_WRITE_ONLY, numWorkGroups * sizeof(int), NULL, &clStatus);
 
-	cl_mem device_reducedWriteTo = clCreateBuffer(context, CL_MEM_WRITE_ONLY, numWorkGroups * sizeof(int), NULL, &clStatus);
+		clSetKernelArg(reduceKernel, 0, sizeof(cl_mem), &device_write_to);
+		clSetKernelArg(reduceKernel, 1, sizeof(cl_int) * localWorkSize, NULL);
+		clSetKernelArg(reduceKernel, 2, sizeof(cl_int), &NUM_VALUES);
+		clSetKernelArg(reduceKernel, 3, sizeof(cl_mem), &device_reducedWriteTo);
 
-    clSetKernelArg(reduceKernel, 0, sizeof(cl_mem), &device_write_to);
-	clSetKernelArg(reduceKernel, 1, sizeof(cl_int) * localWorkSize, NULL);
-	clSetKernelArg(reduceKernel, 2, sizeof(cl_int), &NUM_VALUES);
-	clSetKernelArg(reduceKernel, 3, sizeof(cl_mem), &device_reducedWriteTo);
+		clStatus = clEnqueueNDRangeKernel(command_queue, reduceKernel, 1, NULL, &globalWorkSize, &localWorkSize, 0, NULL, NULL);
 
-	clStatus = clEnqueueNDRangeKernel(command_queue, reduceKernel, 1, NULL, &globalWorkSize, &localWorkSize, 0, NULL, NULL);
+		int *reduced_writeToResult = new int[numWorkGroups];
+		clEnqueueReadBuffer(command_queue, device_reducedWriteTo, CL_TRUE, 0, sizeof(int) * numWorkGroups, reduced_writeToResult, 0, NULL, NULL);
 
-	int *reduced_writeToResult = new int[numWorkGroups];
-	clEnqueueReadBuffer(command_queue, device_reducedWriteTo, CL_TRUE, 0, sizeof(int) * numWorkGroups, reduced_writeToResult, 0, NULL, NULL);
+		int writeToSum = 0;
+		for (size_t i = 0; i < numWorkGroups; i++) {
+			writeToSum += reduced_writeToResult[i];
+		}
 
-	int writeToSum = 0;
-	for (size_t i = 0; i < numWorkGroups; i++) {
-		writeToSum += reduced_writeToResult[i];
-	}
-
-	//printf("writeToSum = %d\n", writeToSum);
-
-
-	// --------------------------------------------------------------------------------------------------------------
-	// ------------------------what the fuck ... need to be refactoried ..................
-	// --------------------------------------------------------------------------------------------------------------
-
-    cl_mem device_reducedWriteCount = clCreateBuffer(context, CL_MEM_WRITE_ONLY, numWorkGroups * sizeof(int), NULL, &clStatus);
-
-    clSetKernelArg(reduceKernel, 0, sizeof(cl_mem), &device_write_count);
-	clSetKernelArg(reduceKernel, 1, sizeof(cl_int) * localWorkSize, NULL);
-	clSetKernelArg(reduceKernel, 2, sizeof(cl_int), &NUM_VALUES);
-	clSetKernelArg(reduceKernel, 3, sizeof(cl_mem), &device_reducedWriteCount);
-
-	clStatus = clEnqueueNDRangeKernel(command_queue, reduceKernel, 1, NULL, &globalWorkSize, &localWorkSize, 0, NULL, NULL);
-
-	int *reducedWriteCountResult = new int[numWorkGroups];
-
-	clEnqueueReadBuffer(command_queue, device_reducedWriteCount, CL_TRUE, 0, sizeof(int) * numWorkGroups, reducedWriteCountResult, 0, NULL, NULL);
-
-	int writeCountSum = 0;
-	for (size_t i = 0; i < numWorkGroups; i++) {
-		writeCountSum += reducedWriteCountResult[i];
-	}
-
-	//printf("writeCountSum = %d\n", writeCountSum);
-	//puts("");
-
-	if (writeToSum < writeCountSum) {
-		spec = 1;
-	}
+		//printf("writeToSum = %d\n", writeToSum);
 
 
-	if (spec == 0) { // launch phase 3 to check read[i] & write[i]
-		clSetKernelArg(DC3kernel, 0, sizeof(cl_mem), &device_read_to);
-		clSetKernelArg(DC3kernel, 1, sizeof(cl_mem), &device_write_to);
-		clSetKernelArg(DC3kernel, 2, sizeof(cl_mem), &device_misspeculation);
-		clSetKernelArg(DC3kernel, 3, sizeof(cl_mem), &NUM_VALUES);
+		// --------------------------------------------------------------------------------------------------------------
+		// ------------------------what the fuck ... need to be refactoried ..................
+		// --------------------------------------------------------------------------------------------------------------
 
-		clEnqueueNDRangeKernel(command_queue, DC3kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
+		cl_mem device_reducedWriteCount = clCreateBuffer(context, CL_MEM_WRITE_ONLY, numWorkGroups * sizeof(int), NULL, &clStatus);
 
-		clEnqueueReadBuffer(command_queue, device_misspeculation, CL_TRUE, 0, sizeof(int), &spec, 0, NULL, NULL);
+		clSetKernelArg(reduceKernel, 0, sizeof(cl_mem), &device_write_count);
+		clSetKernelArg(reduceKernel, 1, sizeof(cl_int) * localWorkSize, NULL);
+		clSetKernelArg(reduceKernel, 2, sizeof(cl_int), &NUM_VALUES);
+		clSetKernelArg(reduceKernel, 3, sizeof(cl_mem), &device_reducedWriteCount);
+
+		clStatus = clEnqueueNDRangeKernel(command_queue, reduceKernel, 1, NULL, &globalWorkSize, &localWorkSize, 0, NULL, NULL);
+
+		int *reducedWriteCountResult = new int[numWorkGroups];
+
+		clEnqueueReadBuffer(command_queue, device_reducedWriteCount, CL_TRUE, 0, sizeof(int) * numWorkGroups, reducedWriteCountResult, 0, NULL, NULL);
+
+		int writeCountSum = 0;
+		for (size_t i = 0; i < numWorkGroups; i++) {
+			writeCountSum += reducedWriteCountResult[i];
+		}
+
+		//printf("writeCountSum = %d\n", writeCountSum);
+		//puts("");
+
+		if (writeToSum < writeCountSum) {
+			spec = 1;
+		}
+
+
+		if (spec == 0) { // launch phase 3 to check read[i] & write[i]
+			clSetKernelArg(DC3kernel, 0, sizeof(cl_mem), &device_read_to);
+			clSetKernelArg(DC3kernel, 1, sizeof(cl_mem), &device_write_to);
+			clSetKernelArg(DC3kernel, 2, sizeof(cl_mem), &device_misspeculation);
+			clSetKernelArg(DC3kernel, 3, sizeof(cl_mem), &NUM_VALUES);
+
+			clEnqueueNDRangeKernel(command_queue, DC3kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
+
+			clEnqueueReadBuffer(command_queue, device_misspeculation, CL_TRUE, 0, sizeof(int), &spec, 0, NULL, NULL);
+
+		}
+
+		delete[] reducedWriteCountResult;
+		delete[] reduced_writeToResult;
 
 	}
 
@@ -283,31 +288,51 @@ static int specTest1NoDependencies(double *used_time) {
 	clReleaseKernel(DC3kernel);
 	clReleaseKernel(reduceKernel);
 
-	delete[] reducedWriteCountResult;
-	delete[] reduced_writeToResult;
 	delete[] emptyIntArray;
 
 	return 0;
 }
 
-static void performCompare() {
+static void performCompare(int testCount = 5) {
 
 	init_assign_mem(NUM_VALUES, FUNC_LOOP_NUM);
 
 	initialize();
 
-	double sequential_time;
-	sequentialTest1(&sequential_time);
+	double seq_time_sum = 0;
+	for (int cnt = 0; cnt < testCount; cnt++) {
+		double sequential_time;
+		sequentialTest1(&sequential_time);
+		seq_time_sum += sequential_time;
+	}
+	double seq_time_avg = seq_time_sum / testCount;
 
 	memcpy(host_A2, host_A, NUM_VALUES * sizeof(float));
 	memcpy(host_B2, host_B, NUM_VALUES * sizeof(float));
 
 	initialize();
 
-	double parallel_time;
-	specTest1NoDependencies(&parallel_time);
+	double par_time_sum = 0;
 
-	printf("%d\t%d\t%.0f\t%.0f\t%.2f\n", NUM_VALUES, FUNC_LOOP_NUM, sequential_time, parallel_time, sequential_time / parallel_time);
+	for (int cnt = 0; cnt < testCount; cnt ++) {
+		double parallel_time;
+		specTest1NoDependencies(&parallel_time, 1);
+		par_time_sum += parallel_time;
+	}
+
+	double par_time_avg = par_time_sum / testCount;
+
+	double par_time_sum_dc_off = 0;
+
+	for (int cnt = 0; cnt < testCount; cnt ++) {
+		double parallel_time_dc_off;
+		specTest1NoDependencies(&parallel_time_dc_off, 0);
+		par_time_sum_dc_off += parallel_time_dc_off;
+	}
+
+	double par_time_avg_dc_off = par_time_sum_dc_off / testCount;
+
+	printf("%d\t%d\t%.0f\t%.0f\t%.0f\t%.2f\t%.2f\n", NUM_VALUES, FUNC_LOOP_NUM, seq_time_avg, par_time_avg, par_time_avg_dc_off, seq_time_avg / par_time_avg, seq_time_avg / par_time_avg_dc_off);
 
 	bool yes = true;
 	for (int i = 0; i < NUM_VALUES; i++) {
@@ -349,7 +374,7 @@ int main (int argc, const char *argv[]) {
 	//puts("input FUNC_LOOP_NUM");
 	//scanf("%d", &FUNC_LOOP_NUM);
 
-	printf("NUM_VALUES\tFUNC_LOOP\tCPU\tGPU\tSpeedUp\t\n");
+	printf("NUM_VALUES\tFUNC_LOOP\tCPU(microsecs)\tGPU(DC on)(microsecs)\tGPU(DC off)(microsecs)\tSpeedUp(DC on)\tSpeedUp(DC off)\n");
 
 	for (int nv = 10000; nv <= 1000000; nv *= 10) {
 		for (int funcIter = 10; funcIter <= 1000; funcIter *= 10) {
