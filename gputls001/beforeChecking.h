b930d4278fa0c45a3667463ed3e56945ee95a9f3
liuhongyuan <lliuhy@gmail.com>
2015-03-09 11:23:00 +0800

    add bc

---
+++ gputls001/beforeChecking.h
@@ -0,0 +1,20 @@
+/*
+ * beforeChecking.h
+ *
+ *  Created on: Mar 1, 2015
+ *      Author: hyliu
+ */
+
+#ifndef GPUTLS001_BEFORECHECKING_H_
+#define GPUTLS001_BEFORECHECKING_H_
+
+
+/*
+ *
+ *
+ *
+ *
+ */
+
+
+#endif /* GPUTLS001_BEFORECHECKING_H_ */
