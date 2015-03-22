171a48e22279553172a4b3c3936a86a2182c3a7a
liuhongyuan <lliuhy@gmail.com>
2015-03-22 23:41:46 +0800

    add antlr generated

---
+++ tlscodegen/src/model/Array.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.List;
+
+public class Array {
+	private int dimension;
+	private String name;
+	private List<Expression> readIndexExprs;
+	private List<Expression> writeIndexExprs;
+	private List<Statement> refferedStatement;
+	
+}
---
+++ tlscodegen/src/model/ArrayType.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum ArrayType {
+	SHARED, NORMAL;
+}
