423a9b5a851b9058ef5de28162a0a0a7d65747f0
liuhongyuan <lliuhy@gmail.com>
2015-05-24 20:50:07 +0800

    test stringtemplate

--- tlscodegen/src/tests/SimpleReadnWriteAnalyzerTest.java
+++ tlscodegen/src/tests/SimpleReadnWriteAnalyzerTest.java
@@ -1,9 +1,10 @@
 package tests;
 
-import java.util.Map;
-
 import jaforloop.parser.JaForLoopLexer;
 import jaforloop.parser.JaForLoopParser;
+
+import java.util.Map;
+
 import model.JaArray;
 import model.JaBlockContext;
 
@@ -16,49 +17,49 @@ import codegen.SimpleReadnWriteAnalyzer;
 import controller.JaVisitor;
 
 public class SimpleReadnWriteAnalyzerTest {
-	
+
 	@Test
 	public void simpleRnWTest1() throws Exception {
-    	JaForLoopLexer lexer = new JaForLoopLexer(new ANTLRFileStream("resources/code_example/1.forloop"));    	
-    	CommonTokenStream tokens = new CommonTokenStream(lexer);
-    	JaForLoopParser parser = new JaForLoopParser(tokens);
-    	ParseTree pt = parser.forloop();
-    	
-    	JaBlockContext ctx = new JaBlockContext(null); //root
-    	JaVisitor visitor = new JaVisitor(ctx);
-    	visitor.visit(pt);
-    	
-    	SimpleReadnWriteAnalyzer ana = new SimpleReadnWriteAnalyzer();
-    	
-    	Map<String, JaArray> arrayInfoMap = ana.getArrayInfos(ctx);
-    	
-    	for (Map.Entry<String, JaArray> e : arrayInfoMap.entrySet()) {
-    		System.out.println(e.getValue());
-    	}
-    	
-    	
+		JaForLoopLexer lexer = new JaForLoopLexer(new ANTLRFileStream("resources/code_example/1.forloop"));
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		JaForLoopParser parser = new JaForLoopParser(tokens);
+		ParseTree pt = parser.forloop();
+
+		JaBlockContext ctx = new JaBlockContext(null); // root
+		JaVisitor visitor = new JaVisitor(ctx);
+		visitor.visit(pt);
+
+		SimpleReadnWriteAnalyzer ana = new SimpleReadnWriteAnalyzer();
+
+		Map<String, JaArray> arrayInfoMap = ana.getArrayInfos(ctx);
+
+		for (Map.Entry<String, JaArray> e : arrayInfoMap.entrySet()) {
+			System.out.println(e.getValue());
+		}
+
 	}
-	
+
 	@Test
 	public void simpleRnWTest2() throws Exception {
-    	JaForLoopLexer lexer = new JaForLoopLexer(new ANTLRFileStream("resources/code_example/2.forloop"));    	
-    	CommonTokenStream tokens = new CommonTokenStream(lexer);
-    	JaForLoopParser parser = new JaForLoopParser(tokens);
-    	ParseTree pt = parser.forloop();
-    	
-    	JaBlockContext ctx = new JaBlockContext(null); //root
-    	JaVisitor visitor = new JaVisitor(ctx);
-    	visitor.visit(pt);
-    	
-    	SimpleReadnWriteAnalyzer ana = new SimpleReadnWriteAnalyzer();
-    	
-    	Map<String, JaArray> arrayInfoMap = ana.getArrayInfos(ctx);
-    	
-    	for (Map.Entry<String, JaArray> e : arrayInfoMap.entrySet()) {
-    		System.out.println(e.getValue());
-    	}
-    	
-    	
+		JaForLoopLexer lexer = new JaForLoopLexer(new ANTLRFileStream("resources/code_example/2.forloop"));
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		JaForLoopParser parser = new JaForLoopParser(tokens);
+		ParseTree pt = parser.forloop();
+
+		JaBlockContext ctx = new JaBlockContext(null); // root
+		JaVisitor visitor = new JaVisitor(ctx);
+		visitor.visit(pt);
+
+		SimpleReadnWriteAnalyzer ana = new SimpleReadnWriteAnalyzer();
+
+		Map<String, JaArray> arrayInfoMap = ana.getArrayInfos(ctx);
+
+		for (Map.Entry<String, JaArray> e : arrayInfoMap.entrySet()) {
+			System.out.println(e.getValue());
+		}
+
 	}
+
+
 
 }
